98c7c9f253929563bcca40241eb3f50af1c1d22d
jannamariyam <75537080+jannamariyam@users.noreply.github.com>
2021-02-09 09:49:08 +0530

    Add files via upload

---
+++ sample2.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<?php
+/* This is a
+ multiple comment*/
+//This is single comment 
+#This is also a single line comment
+$color = "red";
+echo "My house colour is " .$color."<Br>";
+echo "My house colour is " .$COLOR."<Br>";
+echo "My house colour is " .$colOR."<Br>";
+//comment in between the print line
+$x = 5 /* +15 */ +5;
+echo $x;
+?>
+</body>
+</html>
